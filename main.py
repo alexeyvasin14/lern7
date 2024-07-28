3c2d263db5dc635e251ee891c2b86fb37b451405
alexeyvasin14 <vasin14@mail.ru>
2024-07-28 11:32:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+name = 'переменные'
+print(name)
+a = 12
+b = 1.5
+c = 1.5 / 12
+kurse = 'Pyton'
+print('Курс:', kurse,', ''число заданий:', a,', ' 'всего затрачено времени:' , b,', ' 'время на 1 задание:' ,  c)
+
